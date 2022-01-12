af1caa2c48b7b30234589c3c6224a0e9f20d9b45
Nikhil Badyal <nikhill773384@gmail.com>
2022-01-12 17:43:05 +0530

    Added File Field

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
+from profiles.forms import ProfileForm
+from profiles.models import UserProfile
+
+
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            # store_file(request.FILES['user_image'])
+            profile = UserProfile(image=request.FILES['user_image'])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
