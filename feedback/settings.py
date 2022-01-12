71331114bc7a8d049631e5dbf215165725861e5c
Nikhil Badyal <nikhill773384@gmail.com>
2022-01-12 18:00:22 +0530

    Finished FIle Upload

--- feedback/settings.py
+++ feedback/settings.py
@@ -117,6 +117,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 
+MEDIA_URL = "/user-media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import CreateView
+from django.views.generic import ListView
 
-from profiles.forms import ProfileForm
 from profiles.models import UserProfile
 
 
@@ -11,23 +9,34 @@ from profiles.models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+class ProfileView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
 
-        if submitted_form.is_valid():
-            # store_file(request.FILES['user_image'])
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
+#
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#
+#         if submitted_form.is_valid():
+#             store_file(request.FILES['user_image'])
+# profile = UserProfile(image=request.FILES['user_image'])
+# profile.save()
+# return HttpResponseRedirect("/profiles")
+#
+# return render(request, "profiles/create_profile.html", {
+#     "form": submitted_form
+# })
